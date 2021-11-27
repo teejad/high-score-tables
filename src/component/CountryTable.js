1f972d23c55ac65ab4ce92a0290a904db1c66709
Teejad <65988841+teejad@users.noreply.github.com>
2021-11-27 11:41:01 +0000

    Update CountryTable.js

--- src/component/CountryTable.js
+++ src/component/CountryTable.js
@@ -26,23 +26,3 @@ return (
 
 export default CountryTable;
 
-
-// export default function EachScoreTable({ country }) {
-//   country.scores.sort((a, b) => b.s - a.s);
-//   const countryPersons = country.scores.map((person, i) => {
-//     return (
-  //     <tr key={`person${i}`}>
-  //       <th>{person.n}</th>
-  //       <td>{person.s}</td>
-  //     </tr>
-  //   );
-  // });
-  // return (
-  //   <table>
-  //     <thead>
-  //       <tr>
-  //         <th colSpan="2">HIGH SCORE: {country.name}</th>
-  //       </tr>
-  //     </thead>
-  //     <tbody>{countryPersons}</tbody>
-  //   </table>
